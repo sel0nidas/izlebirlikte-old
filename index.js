45c527ac798b7c3d1b13983335cdb35fb5906fe0
Selahattin Emre Kutlu <48654612+MrSelahattin@users.noreply.github.com>
2021-07-27 20:02:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,23 +6,39 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/deneme.html');
+  res.sendFile(__dirname + '/index.html');
 });
+
+var users = {};
+
+var i;
+var index = 0;
 
 io.on('connection', (socket) => {
-  console.log("A user Connected");
-  socket.on('chat message', (msg) => {
-    socket.broadcast.emit(msg);  
+    console.log("A user Connected");
+
     
-    
-    
-  });
-  
+    users[index] = socket.id;
+
+    console.log("Kullanıcı Listesi");
+    for(i=0;i<=index;i++){
+        console.log(index+1+".Kullanıcı: "+users[i]);
+    }
+    socket.on('event message', (msg) => {
+        if(socket.id==users[0])  
+            socket.broadcast.emit('event message', msg);
+            
+    });
+      
+    socket.on('player time', (time) => {
+      if(socket.id==users[0]) 
+          socket.broadcast.emit('player time',time);
+        
+        console.log("Gönderilen Zaman: "+time);
+        
+    });
+    index++;
 });
-
-// client-side
-
-
 
 
 http.listen(port, () => {
